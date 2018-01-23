7457942299fd80a6caad5e5b759fd55d49e54f26
PriyadharshanSaba <priyadharshan.97@gmail.com>
2018-01-23 15:51:20 +0530

    @IBActions

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        stopRecordingButton.isEnabled=false
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text="Recording"
+        stopRecordingButton.isEnabled=true
+        recordButton.isEnabled=false
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text="Tap to record"
+        stopRecordingButton.isEnabled=false
+        recordButton.isEnabled=true
+    }
+
 
 }
 
+/*
+ 2018-01-23 15:34:05.129963+0530 PitchPerfect[5335:267093] -[PitchPerfect.ViewController recordButton:]: unrecognized selector sent to instance 0x7ffb96505b70
+ 2018-01-23 15:34:05.137392+0530 PitchPerfect[5335:267093] *** Terminating app due to uncaught exception 'NSInvalidArgumentException', reason: '-[PitchPerfect.ViewController recordButton:]: unrecognized selector sent to instance 0x7ffb96505b70'
+ *** First throw call stack:
+
+ */
